9b5d49d36855732bd87b3fa75428c40d231177e7
daniyar.kalmurzin <daniyar.kalmurzin@1fa835ee-2271-11de-9b1d-7d4d678aad79>
2009-04-08 12:10:19 +0000

    подправил название переменных

--- trunk/stemmer/src/kz/my/kazakh/stemmer/morphology/Sound.java
+++ trunk/stemmer/src/kz/my/kazakh/stemmer/morphology/Sound.java
@@ -11,75 +11,75 @@ import java.util.List;
  */
 public class Sound {
     // гласные
-    protected List<String> list1;
-    protected List<String> list2;
-    // согласные
-    protected List<String> list3;
-    protected List<String> list4;
+    protected List<String> hardVowels;
+    protected List<String> softVowels;
+    // согласные        consonant
+    protected List<String> deafConsonants;
+    protected List<String> voicedСonsonants;
     // сонорные согласные
-    protected List<String> list5;
+    protected List<String> resonantConsonants;
 
     protected void init1() {
-        list1 = new ArrayList<String>();
+        hardVowels = new ArrayList<String>();
         // твердые гласные
-        list1.add("а");
-        list1.add("о");
-        list1.add("ұ");
-        list1.add("ы");
-        list1.add("э");
-        list1.add("у");    // если употребляется после согласных то он гласный
+        hardVowels.add("а");
+        hardVowels.add("о");
+        hardVowels.add("ұ");
+        hardVowels.add("ы");
+        hardVowels.add("э");
+        hardVowels.add("у");    // если употребляется после согласных то он гласный
     }
 
     protected void init2() {
-        list2 = new ArrayList<String>();
+        softVowels = new ArrayList<String>();
         // мягкие гласные
-        list2.add("ә");
-        list2.add("ө");
-        list2.add("ү");
-        list2.add("і");
-        list2.add("е");
-        list2.add("у");
+        softVowels.add("ә");
+        softVowels.add("ө");
+        softVowels.add("ү");
+        softVowels.add("і");
+        softVowels.add("е");
+        softVowels.add("у");
     }
 
     protected void init3() {
-        list3 = new ArrayList<String>();
+        deafConsonants = new ArrayList<String>();
         // глухие согласные
-        list3.add("п");
-        list3.add("ф");
-        list3.add("к");
-        list3.add("қ");
-        list3.add("т");
-        list3.add("с");
-        list3.add("ш");
-        list3.add("щ");
-        list3.add("ц");
-        list3.add("ч");
-        list3.add("х");
-        list3.add("х");
-        list3.add("Һ");
+        deafConsonants.add("п");
+        deafConsonants.add("ф");
+        deafConsonants.add("к");
+        deafConsonants.add("қ");
+        deafConsonants.add("т");
+        deafConsonants.add("с");
+        deafConsonants.add("ш");
+        deafConsonants.add("щ");
+        deafConsonants.add("ц");
+        deafConsonants.add("ч");
+        deafConsonants.add("х");
+        deafConsonants.add("х");
+        deafConsonants.add("Һ");
     }
 
     protected void init4() {
-        list4 = new ArrayList<String>();
+        voicedСonsonants = new ArrayList<String>();
         // звонкие согласные
-        list4.add("б");
-        list4.add("в");
-        list4.add("г");
-        list4.add("ғ");
-        list4.add("д");
-        list4.add("ж");
-        list4.add("з");
+        voicedСonsonants.add("б");
+        voicedСonsonants.add("в");
+        voicedСonsonants.add("г");
+        voicedСonsonants.add("ғ");
+        voicedСonsonants.add("д");
+        voicedСonsonants.add("ж");
+        voicedСonsonants.add("з");
     }
 
     protected void init5() {
-        list5 = new ArrayList<String>();
+        resonantConsonants = new ArrayList<String>();
         // сонорные согласные
-        list5.add("р");
-        list5.add("л");
-        list5.add("м");
-        list5.add("н");
-        list5.add("ң");
-        list5.add("й");
-        //list5.add("у"); !!!
+        resonantConsonants.add("р");
+        resonantConsonants.add("л");
+        resonantConsonants.add("м");
+        resonantConsonants.add("н");
+        resonantConsonants.add("ң");
+        resonantConsonants.add("й");
+        //resonantConsonants.add("у"); !!!
     }
 }
